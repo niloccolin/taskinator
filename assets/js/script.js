81f0fa518a7641045ec8764ef4be671a17b39f1f
niloccolin <ce59642@gmail.com>
2020-07-09 14:31:33 -0700

    ionno

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+/* var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.apendChild(listItemEl);
-};
+}; */
 
-buttonEl.addEventListener("click", createTaskHandler) {
+buttonEl.addEventListener("click", function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-};
+});

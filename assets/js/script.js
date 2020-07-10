a7e4587f5a876b99b602bc6c9a0265d7e3cc6d16
niloccolin <ce59642@gmail.com>
2020-07-10 11:46:49 -0700

    lol ya

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-/* var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.apendChild(listItemEl);
-}; */
+};
 
-buttonEl.addEventListener("click", function() {
+/* buttonEl.addEventListener("click", function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-});
+}); */
+
+formEl.addEventListener("sumbit", createTaskHandler);
